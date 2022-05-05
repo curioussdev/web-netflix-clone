8bd5bbf2eb20fb8a435fdeb822420493791658e2
Jose Lopes <946393361bill@gmail.com>
2022-05-05 01:14:15 +0100

    conectando mongoDB com mongoosejs

--- netflix-ws/index.js
+++ netflix-ws/index.js
@@ -3,6 +3,20 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const { json } = require('express');
 const routes = require('./src/routes/filmes.routes')
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/netflix', {
+    useNewUrlParser: true,
+    useUnifiedTipology: true,
+});
+
+
+const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => console.log('meow'));
+
+
 
 const app = express();
 app.use(bodyParser.json()); // serve para ajudar o servidor a recuperar exactamente o que está mandando, no caso do insomnia
