f76070834b3344c013a1319a0608ada92fcdb805
Jose Lopes <946393361bill@gmail.com>
2022-05-13 19:33:41 +0100

    addUsers na BD

---
+++ netflix-ws/src/scripts/addUsers.js
@@ -0,0 +1,18 @@
+const database = require('../services/database');
+const Usuario = require('../models/usuario');
+const usuarioJSON = require('../data/usuario.json');
+
+const addUsers = async () => {
+    try {
+        for (let usuario of usuarioJSON) {
+            await new Usuario(usuario).save();
+        }
+        console.log("Final do Script")
+    } catch (err) {
+        console.log(err.message)
+    }
+};
+
+addUsers();
+
+
--- netflix-ws/src/services/database.js
+++ netflix-ws/src/services/database.js
@@ -1,2 +1,3 @@
 const mongoose = require('mongoose'); // é uma ferramenta/camada js para trabalhar com o mongoDB
 
+mongoose.connect('mongodb://localhost:27017/netflix');         
