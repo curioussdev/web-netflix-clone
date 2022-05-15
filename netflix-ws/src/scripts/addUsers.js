0c62acd8429c11fcb57a8d0c83b44f13cd87e16a
Jose Lopes <946393361bill@gmail.com>
2022-05-15 21:02:00 +0100

    resume

--- netflix-ws/src/scripts/addUsers.js
+++ netflix-ws/src/scripts/addUsers.js
@@ -5,13 +5,14 @@ const usuarioJSON = require('../data/usuario.json');
 const addUsers = async () => {
     try {
         for (let usuario of usuarioJSON) {
+            console.log(`Inserido ${usuario.nome}`)
             await new Usuario(usuario).save();
         }
         console.log("Final do Script")
     } catch (err) {
         console.log(err.message)
     }
-};
+}; 
 
 addUsers();
 
