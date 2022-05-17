af45ea3a5bafb0bc9a9199fa6325576b109082c3
Jose Lopes <946393361bill@gmail.com>
2022-05-17 22:32:27 +0100

    iniciando cors

--- netflix-ws/index.js
+++ netflix-ws/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const database = require('./src/services/database');
-
+const cors = require('cors');
 const routes = require('./src/routes/filmes.routes');
 
 const app = express();
 app.use(bodyParser.json()); // serve para ajudar o servidor a recuperar exactamente o que está mandando, no caso do insomnia
+app.use(cors()); // para control de acesso no front
 app.use(morgan('dev')); // morgan serve para visualizar as rotas acessadas
 app.use('/', routes);
 
